004a73e803f757eb4bfd4e56f1f14a1ff1ba6bef
Mike - Ubuntu <mpm.developer@gmail.com>
2017-09-22 10:00:32 -0400

    Add two integers and displayed result!!!

--- src/main/java/com/gmail/developer/mpm/swing001/App.java
+++ src/main/java/com/gmail/developer/mpm/swing001/App.java
@@ -1,13 +1,23 @@
 package com.gmail.developer.mpm.swing001;
 
+import javax.swing.JOptionPane;
+
 /**
- * Hello world!
+ * Simple app that adds two numbers
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String firstNumber = JOptionPane.showInputDialog("Enter first integer: ");
+        String secondNumber = JOptionPane.showInputDialog("Enter second integer:");
+        
+        int a = Integer.parseInt(firstNumber);
+        int b = Integer.parseInt(secondNumber);
+        
+        int c = a + b;
+        
+        JOptionPane.showMessageDialog(null, "The sume is: " + c, "Sum of two integers", JOptionPane.PLAIN_MESSAGE);
     }
 }
